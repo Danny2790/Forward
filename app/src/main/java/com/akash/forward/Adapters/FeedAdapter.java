bb5f5c80a40349a09c8a358e9e42c0d34d0c7cad
Akash Danao <akash@aitglobalinc.com>
2017-08-20 23:19:57 +0530

    like unlike implementation is done

--- app/src/main/java/com/akash/forward/Adapters/FeedAdapter.java
+++ app/src/main/java/com/akash/forward/Adapters/FeedAdapter.java
@@ -13,46 +13,83 @@ import com.akash.forward.Models.Feed;
 import com.akash.forward.R;
 import com.akash.forward.Utility.Utils;
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-import static com.akash.forward.Constants.ForwardConstant.FIREBASE_DB_REF;
+import static com.akash.forward.Constants.ForwardConstant.FIREBASE_DB_FEEDS;
+import static com.akash.forward.Constants.ForwardConstant.FIREBASE_DB_LIKES;
 
 /**
  * Created by akash on 8/20/2017.
  */
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+    private final FirebaseAuth firebaseAuth;
     private ArrayList<Feed> feedList;
     private Context context;
     private String TAG = FeedAdapter.class.getSimpleName();
     private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mFirebaseDatabaseReference;
+    private DatabaseReference mFirebaseDatabaseLikes;
+    private DatabaseReference mFirebaseDatabaseFeeds;
+    private Boolean mProcessLike = false;
 
     public FeedAdapter(Context context, ArrayList<Feed> feedList) {
         this.context = context;
         this.feedList = feedList;
         mFirebaseDatabase = Utils.getDatabase();
-        mFirebaseDatabaseReference = mFirebaseDatabase.getReference(FIREBASE_DB_REF);
+        mFirebaseDatabaseFeeds = mFirebaseDatabase.getReference(FIREBASE_DB_FEEDS);
+        mFirebaseDatabaseLikes = mFirebaseDatabase.getReference(FIREBASE_DB_LIKES);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Log.d(TAG, "onCreateViewHolder: " + parent + " feed layout : " + R.layout.feed_item);
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Feed feed = feedList.get(position);
+        final Feed feed = feedList.get(position);
         holder.textViewUsername.setText(feed.getUserInfo().getFirstName());
-        Log.d(TAG, "onBindViewHolder: " + feed.getImageUrl());
         Glide.with(context)
                 .load(feed.getImageUrl())
                 .into(holder.imageViewFeed);
+        Log.d(TAG, "onBindViewHolder:  post id " + feed.getPostId());
+        holder.setLikeButton(feed.getPostId());
+        holder.imageViewLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProcessLike = true;
+                mFirebaseDatabaseLikes.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (mProcessLike) {
+                            if (dataSnapshot.child(feed.getPostId()).hasChild(firebaseAuth.getCurrentUser().getUid())) {
+                                mFirebaseDatabaseLikes.child(feed.getPostId()).child(firebaseAuth.getCurrentUser().getUid()).removeValue();
+                                mProcessLike = false;
+                            } else {
+                                mFirebaseDatabaseLikes.child(feed.getPostId()).child(firebaseAuth.getCurrentUser().getUid()).setValue(true);
+                                mProcessLike = false;
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
@@ -62,7 +99,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView textViewUsername, textViewLikeCount, textViewCommentCount;
-        ImageView imageViewFeed;
+        ImageView imageViewFeed, imageViewLike, imageViewComments;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -70,6 +107,26 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
             textViewLikeCount = (TextView) itemView.findViewById(R.id.tv_like_count);
             textViewCommentCount = (TextView) itemView.findViewById(R.id.tv_comment_count);
             imageViewFeed = (ImageView) itemView.findViewById(R.id.iv_feed_image);
+            imageViewLike = (ImageView) itemView.findViewById(R.id.iv_like);
+            imageViewComments = (ImageView) itemView.findViewById(R.id.iv_comment);
+        }
+
+        public void setLikeButton(final String post_id) {
+            mFirebaseDatabaseLikes.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.child(post_id).hasChild(firebaseAuth.getCurrentUser().getUid())) {
+                        imageViewLike.setImageResource(R.drawable.ic_thumb_up_red_24px);
+                    } else {
+                        imageViewLike.setImageResource(R.drawable.ic_thumb_up_black_24px);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 }
